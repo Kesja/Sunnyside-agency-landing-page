d6fbf6354480a079e5850d82c20b6e5a4f8ee9fe
Joanna Bach <joannakatarzynabach@gmail.com>
2022-08-12 15:25:12 +0200

    add more media queries

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const navToggle = document.querySelector('#navToggle');
 const mobileNav = document.querySelector('nav');
+const title = document.querySelector('.title');
 
 navToggle.addEventListener('click', () => {
   mobileNav.classList.toggle('active');
+  title.classList.toggle('hide');
 });
